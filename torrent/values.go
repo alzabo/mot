71be31ec768a9a809d9c5ff369bb20aabab7aec0
Ryan White <ryan@alzabo.io>
2025-01-15 22:52:55 -0500

    Use generic Value struct

--- torrent/values.go
+++ torrent/values.go
@@ -2,7 +2,6 @@ package torrent
 
 import (
 	"fmt"
-	"slices"
 )
 
 const (
@@ -18,99 +17,121 @@ type Values interface {
 }
 
 type Value interface {
+	Raw() any
 	String() string
 }
 
-type InfoValues struct {
-	info         Info
-	stringValues map[string]Value
+type valueWrapper struct {
+	item    any
+	mapping map[string]Value
 }
 
-func (v InfoValues) Get(s string) Value {
-	val, ok := v.stringValues[s]
+func (v valueWrapper) Get(s string) Value {
+	val, ok := v.mapping[s]
 	if !ok {
 		return nil
 	}
 	return val
 }
 
-func (v InfoValues) Keys() []string {
-	keys := []string{}
-	for k := range v.stringValues {
+func (v valueWrapper) Keys() []string {
+	keys := make([]string, 0, len(v.mapping))
+	for k := range v.mapping {
 		keys = append(keys, k)
 	}
-	slices.Sort(keys)
 	return keys
 }
 
-type stringValue struct {
-	v string
+type val struct {
+	value   any
+	strFunc func(any) string
 }
 
-func (v stringValue) String() string {
-	return v.v
+func (v val) Raw() any {
+	return v.value
 }
 
-type percentValue struct {
-	v float64
+func (v val) String() string {
+	if v.strFunc != nil {
+		return v.strFunc(v.value)
+	}
+	f := "%v"
+	switch v.value.(type) {
+	case int, int64:
+		f = "%d"
+	case float64:
+		f = "%.2f"
+	case string, []byte:
+		f = "%s"
+	}
+	return fmt.Sprintf(f, v.value)
 }
 
-func (v percentValue) String() string {
-	return fmt.Sprintf("%.2f%%", v.v*100)
-}
-
-type bytesValue struct {
-	v int64
-}
-
-func (v bytesValue) String() string {
-	if v.v < KiB {
-		return fmt.Sprintf("%d B", v.v)
-	} else if v.v < MiB {
-		return fmt.Sprintf("%.2f KiB", float64(v.v)/float64(KiB))
-	} else if v.v < GiB {
-		return fmt.Sprintf("%.2f MiB", float64(v.v)/float64(MiB))
-	} else if v.v < TiB {
-		return fmt.Sprintf("%.2f GiB", float64(v.v)/float64(GiB))
-	} else {
-		return fmt.Sprintf("%.2f TiB", float64(v.v)/float64(TiB))
+func fmtPercent(a any) string {
+	switch v := a.(type) {
+	case float64:
+		return fmt.Sprintf("%.2f%%", v*100)
+	default:
+		panic("unreachable")
 	}
 }
 
-type rateValue struct {
-	v int64
+func fmtRate(a any) string {
+	switch v := a.(type) {
+	case int64:
+		if v < KiB {
+			return fmt.Sprintf("%d B/s", v)
+		} else if v < MiB {
+			return fmt.Sprintf("%.2f KiB/s", float64(v)/float64(KiB))
+		} else if v < GiB {
+			return fmt.Sprintf("%.2f MiB/s", float64(v)/float64(MiB))
+		} else if v < TiB {
+			return fmt.Sprintf("%.2f GiB/s", float64(v)/float64(GiB))
+		} else {
+			return fmt.Sprintf("%.2f TiB/s", float64(v)/float64(TiB))
+		}
+	default:
+		panic("unreachable")
+	}
 }
 
-func (v rateValue) String() string {
-	if v.v < KiB {
-		return fmt.Sprintf("%d B/s", v.v)
-	} else if v.v < MiB {
-		return fmt.Sprintf("%.2f KiB/s", float64(v.v)/float64(KiB))
-	} else if v.v < GiB {
-		return fmt.Sprintf("%.2f MiB/s", float64(v.v)/float64(MiB))
-	} else if v.v < TiB {
-		return fmt.Sprintf("%.2f GiB/s", float64(v.v)/float64(GiB))
-	} else {
-		return fmt.Sprintf("%.2f TiB/s", float64(v.v)/float64(TiB))
+func fmtBytes(a any) string {
+	switch v := a.(type) {
+	case int64:
+		if v < KiB {
+			return fmt.Sprintf("%d B", v)
+		} else if v < MiB {
+			return fmt.Sprintf("%.2f KiB", float64(v)/float64(KiB))
+		} else if v < GiB {
+			return fmt.Sprintf("%.2f MiB", float64(v)/float64(MiB))
+		} else if v < TiB {
+			return fmt.Sprintf("%.2f GiB", float64(v)/float64(GiB))
+		} else {
+			return fmt.Sprintf("%.2f TiB", float64(v)/float64(TiB))
+		}
+	default:
+		panic("unreachable")
 	}
 }
 
 // Explicitly maps keys to value representations
 func (i Info) Values() Values {
-	return InfoValues{
-		info: i,
-		stringValues: map[string]Value{
-			"name":       stringValue{i.Name},
-			"hash":       stringValue{i.Hash},
-			"state":      stringValue{i.State},
-			"progress":   percentValue{i.Progress},
-			"size":       bytesValue{i.Size},
-			"size_raw":   stringValue{fmt.Sprintf("%d", i.Size)},
-			"tags":       stringValue{i.Tags},
-			"category":   stringValue{i.Category},
-			"speed_down": rateValue{i.Dlspeed},
-			"speed_up":   rateValue{i.Upspeed},
-			"tracker":    stringValue{i.Tracker},
+	return valueWrapper{
+		item: i,
+		mapping: map[string]Value{
+			"name":       val{value: i.Name},
+			"hash":       val{value: i.Hash},
+			"state":      val{value: i.State},
+			"size":       val{value: i.Size, strFunc: fmtBytes},
+			"tags":       val{value: i.Tags},
+			"category":   val{value: i.Category},
+			"speed_down": val{value: i.Dlspeed, strFunc: fmtRate},
+			"speed_up":   val{value: i.Upspeed, strFunc: fmtRate},
+			"tracker":    val{value: i.Tracker},
+			"progress": val{
+				value:   i.Progress,
+				strFunc: fmtPercent,
+			},
 			// dateadded
 			// datelastactive
 			// downloaded
@@ -121,41 +142,23 @@ func (i Info) Values() Values {
 	}
 }
 
-type valueWrapper struct {
-	item         any
-	stringValues map[string]Value
-}
-
-func (v valueWrapper) Get(s string) Value {
-	val, ok := v.stringValues[s]
-	if !ok {
-		return nil
-	}
-	return val
-}
-
-func (v valueWrapper) Keys() []string {
-	keys := []string{}
-	for k := range v.stringValues {
-		keys = append(keys, k)
-	}
-	slices.Sort(keys)
-	return keys
-}
-
 func (f File) Values(vals map[string]string) Values {
 	w := valueWrapper{
 		item: f,
-		stringValues: map[string]Value{
-			"name":     stringValue{f.Name},
-			"size":     bytesValue{f.Size},
-			"size_raw": stringValue{fmt.Sprintf("%d", f.Size)},
-			"progress": percentValue{f.Progress},
-			"index":    stringValue{fmt.Sprintf("%d", f.Index)},
+		mapping: map[string]Value{
+			"name":     val{value: f.Name},
+			"size":     val{value: f.Size, strFunc: fmtBytes},
+			"progress": val{value: f.Progress, strFunc: fmtPercent},
+			"index": val{
+				value: f.Index,
+				strFunc: func(a any) string {
+					return fmt.Sprintf("%d", f.Index)
+				},
+			},
 		},
 	}
 	for k, v := range vals {
-		w.stringValues[k] = stringValue{v}
+		w.mapping[k] = val{value: v}
 	}
 	return w
 }
