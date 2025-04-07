5eb95c5808b3690331cda962032d82da1609135c
Ryan White <ryan@alzabo.io>
2025-04-06 23:35:24 -0400

    Add flags for debugging and move into cobra

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,11 @@
 package cmd
 
 import (
+	"log"
 	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -23,6 +27,8 @@ import (
 
 var cfgFile string
 var outputFormat string
+var cpuProfile string
+var memProfile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -37,6 +43,43 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if debug, _ := cmd.Flags().GetBool("debug"); !debug {
+			return
+		}
+		f, err := os.Create(cpuProfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		go func() {
+			<-c
+			f.Close()
+			os.Exit(0)
+		}()
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		if debug, _ := cmd.Flags().GetBool("debug"); !debug {
+			return
+		}
+
+		// Stop CPU profiling
+		pprof.StopCPUProfile()
+
+		f, err := os.Create(memProfile)
+		if err != nil {
+			log.Fatal("could not create memory profile:", err)
+		}
+
+		defer f.Close()
+		runtime.GC()
+
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile:", err)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -62,6 +105,12 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVarP(&outputFormat, "output", "o", "", "output format")
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mot.yaml)")
+
+	rootCmd.PersistentFlags().StringVar(&cpuProfile, "profile-cpu", "cpu.prof", "write cpu profiles to file")
+	rootCmd.PersistentFlags().MarkHidden("profile-cpu")
+	rootCmd.PersistentFlags().StringVar(&memProfile, "profile-mem", "mem.prof", "write memory profiles to file")
+	rootCmd.PersistentFlags().MarkHidden("profile-mem")
+	rootCmd.PersistentFlags().Bool("debug", false, "enable debugging for mot")
 
 	rootCmd.AddGroup(
 		&cobra.Group{ID: "basic", Title: "Basic Commands:"},
